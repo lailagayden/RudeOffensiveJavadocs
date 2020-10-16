e029940bc9a5cb2814e3a440543e96cde6a31ee8
Laila Gayden <lmgdarling@gmail.com>
2020-10-16 20:54:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Write an algorithm that prints the elements of an array in reverse order.
+a = [0,1,2,3,4,5,6,7,8,9]
+for i in range(9,-1,-1):
+  print(a[i])
+for n in range(10,22,2):
+  print(n)
+for k in range(0,120,20):
+  print(k)
+  b= [2,4,6,8,10,12]
+  for g in range(12,0,2):
+    print(b[g])
